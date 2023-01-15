c9bd5aae5b6830f2e0acddf2d5ea000d0242dc3e
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-15 15:29:38 -0700

    updated indices, api routes

---
+++ routes/api/thoughts.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController')
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+router
+    .route('/:id')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction)
+
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getSingleUser,
+    addUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController')
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(addUser);
+
+router
+    .route('/:id')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+
+module.exports = router;
